ed5f637137ac91c910baab15528edf7b2b13cb15
Ankita Ghosh <91538328+Phoenixcoder-6@users.noreply.github.com>
2025-10-04 21:18:02 +0530

    Add files via upload

---
+++ Week-5 - Dynamic Programming Problems/Fibonacci_Tabulation.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    if n<=1:
+        return n
+    dp= [0] * (n+1)
+    dp[0],dp[1]= 0,1
+
+    for i in range(2, n+1):
+        dp[i]= dp[i-1] + dp[i-2]
+
+    return dp[n]
+
+
+res= [ fibonacci(i) for i in range(7)]
+print(res)
